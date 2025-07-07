89908bccc5c4f2b5494cb2d6866468f06375358b
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2025-07-07 16:36:32 +1000

    next: Fixed a bug

--- lua/utils.lua
+++ lua/utils.lua
@@ -17,18 +17,22 @@ function M.run_command(cwd, command)
             print("Terminal channel is not open.")
         end
     else
-        -- Open a new terminal split
-        vim.cmd("split | terminal")
-        M._terminal_win = vim.api.nvim_get_current_win()
-        M._terminal_chan_id = vim.b.terminal_job_id
-        -- Send the cd and command to the terminal
-        vim.cmd.wincmd("J")
-        vim.cmd('resize 20')
-        vim.fn.chansend(M._terminal_chan_id, {"cd " .. cwd, ""})
-        vim.fn.chansend(M._terminal_chan_id, {command, ""})
-        vim.cmd("normal! G")
-        -- Reselect the previous buffer
-        vim.api.nvim_set_current_win(curr_win)
+        -- Without defer, the terminal sometimes acts weirdly.
+        -- This is related to the fact that the terminal is not fully initialized.
+        vim.defer_fn(function()
+            -- Open a new terminal split
+            vim.cmd("split | terminal")
+            M._terminal_win = vim.api.nvim_get_current_win()
+            M._terminal_chan_id = vim.b.terminal_job_id
+
+            vim.cmd('resize 20')
+            vim.cmd.wincmd("J")
+            vim.fn.chansend(M._terminal_chan_id, {"cd " .. cwd, ""})
+            vim.fn.chansend(M._terminal_chan_id, {command, ""})
+            vim.cmd("normal! G")
+            -- Reselect the previous buffer
+            vim.api.nvim_set_current_win(curr_win)
+        end, 100)
     end
 end
 
