42f9fb84e5478e6517927e298ea324ba2c00d52f
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2025-07-08 10:10:25 +1000

    next: Error check

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,7 +3,19 @@ local M = {}
 M._terminal_win = nil
 
 M._execute_cmd = function(cwd, cmd, curr_win)
-    vim.fn.jobstart(cmd, {cwd = cwd, term=true})
+    vim.fn.jobstart(cmd, {cwd = cwd, term=true,
+        on_exit = function()
+            -- Process the buffer line by line and look for errors
+            local lines = vim.api.nvim_buf_get_lines(M._terminal_win, 0, -1, false)
+            local errors = {}
+
+            for _, line in ipairs(lines) do
+                if string.match(line, ":%d+:%d+: error:") then
+                    table.insert(errors, line)
+                end
+            end
+            print("Errors found: " .. #errors)
+        end})
     vim.api.nvim_set_current_win(curr_win)
 end
 
