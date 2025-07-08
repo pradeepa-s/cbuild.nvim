2b8ea6685bb492f1a207c6b889495e7362ec11f8
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2025-07-08 10:16:59 +1000

    next: Added quickfix list

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,20 +1,35 @@
 local M = {}
 
 M._terminal_win = nil
+M._last_cmd = nil
+
+M._get_errors = function()
+            -- Process the buffer line by line and look for errors
+    local lines = vim.api.nvim_buf_get_lines(M._terminal_win, 0, -1, false)
+    local errors = {}
+
+    for _, line in ipairs(lines) do
+        if string.match(line, ":%d+:%d+: error:") then
+            table.insert(errors, line)
+        end
+    end
+    return errors
+end
+
+M._populate_quickfix = function(errors)
+    -- Add the errors to the quickfix list
+    if #errors > 0 then
+        vim.fn.setqflist({}, 'r', {title = 'Compile errors ' .. M._cmd, lines = errors})
+        vim.cmd('copen')
+    end
+end
 
 M._execute_cmd = function(cwd, cmd, curr_win)
+    M._last_cmd = cmd
     vim.fn.jobstart(cmd, {cwd = cwd, term=true,
         on_exit = function()
-            -- Process the buffer line by line and look for errors
-            local lines = vim.api.nvim_buf_get_lines(M._terminal_win, 0, -1, false)
-            local errors = {}
-
-            for _, line in ipairs(lines) do
-                if string.match(line, ":%d+:%d+: error:") then
-                    table.insert(errors, line)
-                end
-            end
-            print("Errors found: " .. #errors)
+            local errors = M._get_errors()
+            M._populate_quickfix(errors)
         end})
     vim.api.nvim_set_current_win(curr_win)
 end
